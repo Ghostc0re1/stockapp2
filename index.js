519495b4301aa7f69f1b23839103b0c116b6d2b7
Taylor Berardelli <tberardelli@fortpittcapital.com>
2022-12-20 15:13:33 -0500

    added api info

--- index.js
+++ index.js
@@ -1,19 +1,43 @@
+//stock market portfolio app by Taylor Berardelli
+
+const request = require('request');
 const express = require('express');
 const app = express();
 const exphbs = require('express-handlebars');
 const path = require('path');
 
 const PORT = process.env.PORT || 5000;
+//call api
+function call_api(finishedAPI){
+    request('https://api.iex.cloud/v1/data/core/quote/aapl?token=pk_59727eca654d475bb7df57f100dcdf58', { json: true }, (err, res, body) => {
+    if(err) {return console.log(err);}
+    if(res.statusCode === 200) {
+        finishedAPI(body);   
+        };
+    });
+};
 
 //set handlebars middleware
 app.engine('handlebars', exphbs.engine());
 app.set('view engine', 'handlebars');
 
-//set handlebars
-app.get('/', (req, res) => {
+//set handlebars routes
+app.get('/', function(req, res) {
+    call_api(function(doneAPI) {
         res.render('home', {
-            stuff: "this is Stuff"
+            stock: doneAPI
+        });
     });
+});
+
+//create about page
+app.get('/info.html', (req, res) => {
+    res.render('info')
+});
+
+//create home page
+app.get('/main.html', (req, res) => {
+    res.render('home')
 });
 
 //set static folder
